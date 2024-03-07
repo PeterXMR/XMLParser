8b91a62ccd74e156171c01a0badb3c8ffcd66cb8
peterxmr <peter.kozuch@zoho.com>
2024-03-07 19:34:37 +0100

    Constants fix

--- src/main/java/com/xml/parser/Constants.java
+++ src/main/java/com/xml/parser/Constants.java
@@ -4,12 +4,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Constants {
-    private static final String SOURCE_ZIP = "/Users/accountname/Desktop/parser/kopidlno.xml.zip";
-    private static final String TARGET_FOLDER = "/Users/accountname/Desktop/parser/unzipped";
+    private static final String SOURCE_ZIP = "kopidlno.xml.zip";
+    private static final String TARGET_FOLDER = "unzipped";
     public static final Path SOURCE_ZIP_PATH = Paths.get(SOURCE_ZIP);
     public static final Path TARGET_FOLDER_PATH = Paths.get(TARGET_FOLDER);
     public static final String XML_FILE_NAME = "20210331_OB_573060_UZSZ.xml";
     public static final String ZIP_FILE_NAME = "kopidlno.xml.zip";
-    public static final String XML_FILE_PATH = "/Users/accountname/Desktop/parser/unzipped/20210331_OB_573060_UZSZ.xml";
+    public static final String XML_FILE_PATH = "unzipped/20210331_OB_573060_UZSZ.xml";
 
 }
